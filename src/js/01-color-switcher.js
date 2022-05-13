1e2f9648729a42a135ad42d82055060a8477b228
TetianaSosiedka <tasha.sosedka@gmail.com>
2022-05-13 22:21:29 +0300

    finished task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  buttonStart: document.querySelector('button[data-start]'),
+  buttonStop: document.querySelector('button[data-stop]'),
+};
+let timerId = null;
+let buttonDisabled = 'disabled';
+refs.buttonStart.addEventListener('click', onChangeColor);
+refs.buttonStop.addEventListener('click', onStopChangeColor);
 
+//-------------------------------------------------------------
+function onChangeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.buttonStart.toggleAttribute('disabled');
+  if (refs.buttonStop.hasAttribute('disabled')) {
+    refs.buttonStop.toggleAttribute('disabled');
+  }
+}
+function onStopChangeColor() {
+  clearInterval(timerId);
+  refs.buttonStop.toggleAttribute('disabled');
+  if (refs.buttonStart.hasAttribute('disabled')) {
+    refs.buttonStart.toggleAttribute('disabled');
+  }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
